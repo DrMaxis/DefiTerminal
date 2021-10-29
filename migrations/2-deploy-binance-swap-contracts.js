510426385563583894180812a6d68b66fcda2b13
Alchemist <nathankantwi@gmail.com>
2021-10-29 00:18:52 -0600

    binance token swap contract migration script

---
+++ migrations/2-deploy-binance-swap-contracts.js
@@ -0,0 +1,9 @@
+const ApeTokenSwap = require('../build/contracts/ApeTokenSwap.json');
+const BakeryTokenSwap = require('../build/contracts/BakeryTokenSwap.json');
+const PancakeTokenSwap = require('../build/contracts/PancakeTokenSwap.json');
+
+module.exports = function (deployer) {
+  //deployer.deploy(ApeTokenSwap);
+  //deployer.deploy(BakeryTokenSwap);
+  //deployer.deploy(PancakeTokenSwap);
+};
