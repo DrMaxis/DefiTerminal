2b55bdd7baff972079f756c68ec6e67b12580c46
Alchemist <5355808+DrMaxis@users.noreply.github.com>
2021-10-28 22:09:29 -0600

    update the migration script with potential arbitrage contract deployments (#18)

--- migrations/1-deploy-contracts.js
+++
@@ -1,6 +0,0 @@
-const sushiUni = artifacts.require("SushiUniFlashloan");
-const { kovan } = require('../program/utils/addresses/kovan');
-
-module.exports = function(deployer, _network, [beneficiaryAddress, _]) {
-
-};
